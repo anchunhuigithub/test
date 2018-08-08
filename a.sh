0e648595347270e29adcccc0f694ce3aa0ef1a9b
anchunhui <anchunhui_qi@163.com>
2018-08-08 20:06:08 +0800

    add something to the about hive reduce and linux date

--- a.sh
+++ a.sh
@@ -4,3 +4,6 @@ date_now=`date +"%Y-%m-%d %H:%M:%S"`
 echo $date_now
 date_tomorrow=`date -d "2 days ago" +"%Y-%m-%d %H:%M:%S"`
 echo 两天前的时间为:"$date_tomorrow"
+# 得到两天后的时间
+data_two_days_after=`date -d "-2 days ago"+"%Y-%m-%d"`
+
